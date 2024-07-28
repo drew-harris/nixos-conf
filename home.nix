b910c1972df5ccf89378bbd5f8cff178a87dad42
Drew Harris <harrisd@smu.edu>
2024-07-28 15:45:57 -0400

    add discord

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
     # feel free to add your own or remove some of them
+    discord
 
     neofetch
     #nnn # terminal file manager
